fedf67e2c4479409e03bc66b56d210a265c44dab
twongkee <twongkee@gmail.com>
2022-01-15 16:00:51 -0500

    Create mkdiff.py

---
+++ python/mkdiff.py
@@ -0,0 +1,42 @@
+import os
+import PIL.Image as Image
+import PIL.ImageChops as ImageChops
+
+
+
+def compare_images(path_one, path_two, diff_save_location):
+    """
+    Compares to images and saves a diff image, if there
+    is a difference
+    
+    @param: path_one: The path to the first image
+    @param: path_two: The path to the second image
+    """
+    image_one = Image.open(path_one)
+    image_two = Image.open(path_two)
+    
+    diff = ImageChops.difference(image_one, image_two)
+    
+    if diff.getbbox():
+        diff.save(diff_save_location)
+
+        
+if __name__ == '__main__':
+    #compare_images('left.jpg',
+    #               'right.jpg',
+    #               'diff.jpg')
+    fname="bd.txt"
+
+    flist = []
+    with open (fname,"r") as f:
+        for line in f:
+            flist.append(line.rstrip()) 
+
+    n = len(flist)
+
+    d = 1
+    while d < n:
+        savefile=f"diff_{d:04}.jpg"
+        compare_images(flist[d], flist[d-1], savefile)
+        print(savefile)
+        d+=1
